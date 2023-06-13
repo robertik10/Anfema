15270f91fe47c99b7f7773b03cb2d16e6db60828
Robert <robertfischer654@gmail.com>
2023-06-13 17:09:42 +0200

    Django Admin erstellt, scheint bisher alles zu funktionieren

--- anfema_projects/models.py
+++ anfema_projects/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+# model für daten aus der JSON Datei https://www.anfe.ma/api/v2/projects/?format=json&locale=en
+class AnfemaPorject(models.Model):
+    id = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=200)
+    meta_first_published_at = models.DateTimeField("date published")
+    client = models.CharField(max_length=200)
+    subtitle = models.CharField(max_length=200)
+    brand_main_color = models.CharField(max_length=200)
+    updated_at = models.DateTimeField(verbose_name="Last Updated",auto_now = True, blank=True)  #TODO: why should this be blank?
+     
+    def __str__(self):
+        return self.title
+    
