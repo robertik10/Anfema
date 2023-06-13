12ae0f1359d9585dc0b6dddf2078374d99838f1f
Robert <robertfischer654@gmail.com>
2023-06-13 17:37:16 +0200

    3.1 last-update Vorlage erstellt -> dort weitermachen

--- anfema_projects/admin.py
+++ anfema_projects/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from .models import AnfemaPorject
 
 
-
 class AnfemaProjectAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'meta_first_published_at', 'client', 'subtitle', 'brand_main_color', 'updated_at')
 
